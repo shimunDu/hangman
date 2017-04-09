cdabafc3a76bf90a16dc13ee5954e83f4daecaee
shimunDu <teo.nenadic@gmail.com>
2017-04-09 22:18:33 +0200

    Some css added

--- js/hangman.js
+++ js/hangman.js
@@ -1,43 +1,115 @@
+var leftArmHtml = '<div class=\'leftarm\'></div>';
+var rightArmHtml = '<div class=\'rightarm\'></div>';
+var torsoHtml = '<div class=\'torso\'></div>';
+var leftLegHtml = '<div class=\'leftleg\'></div>';
+var rightLegHtml = '<div class=\'rightleg\'></div>';
+var pelvisHtml = '<div class=\'pelvis\'></div>';
+var headHtml = '<div class=\'head\'> </div>';
+
+var brojPromasaja = 0;
+var maxBrojPromasaja = 7;
+
 function ispisiPoljeNepogodenih() {
     document.getElementById('hangman_print').innerHTML = poljeNepogodenih.join(" ");
 }
+
+
 
 function hangmanSubmit(form) {
 
     var input = form.inputbox.value;
     updateajPoljeNepogodenih(input);
     ispisiPoljeNepogodenih();
+    iscrtajCoeka();
+
 }
 
-function updateajPoljeNepogodenih(novoSlovo){
+function iscrtajCoeka() {
 
-  for(var i = 0; i<film.length;i++){
-    if(novoSlovo==film[i]){
-      poljeNepogodenih[i] = film [i];
-      --brojNepogodenihSlova;
+    if (0 == brojPromasaja)
+        return;
+
+    //crtaj glavu
+    document.getElementById("headPlaceholder").innerHTML = headHtml;
+
+    //ako je broj promasaja samo jedan prekidaj funkciju nakon
+    //isctavanja glave
+    if (1 == brojPromasaja)
+        return;
+
+
+    armboxElements = document.getElementsByClassName("armbox");
+    armboxElements[0].innerHTML = rightArmHtml;
+
+    if (2 == brojPromasaja)
+        return;
+
+    armboxElements[0].innerHTML += leftArmHtml;
+    if (3 == brojPromasaja)
+        return;
+
+
+    armboxElements[0].innerHTML += torsoHtml;
+    if (4 == brojPromasaja)
+        return;
+
+    legElements = document.getElementsByClassName("legbox");
+    legElements[0].innerHTML = pelvisHtml;
+
+    if (5 == brojPromasaja)
+        return;
+
+    legElements[0].innerHTML += leftLegHtml;
+    if (6 == brojPromasaja)
+        return;
+
+    legElements[0].innerHTML += rightLegHtml;
+
+    if (7 == brojPromasaja) {
+      setTimeout(function() { alert("*******Popusio si************"); },
+      1000);
     }
-  }
+
+
+
+}
+
+
+function updateajPoljeNepogodenih(novoSlovo) {
+
+    var brojNepogodenihSlovaNaPocetku = brojNepogodenihSlova;
+    for (var i = 0; i < film.length; i++) {
+        if (novoSlovo == film[i]) {
+            poljeNepogodenih[i] = film[i];
+            --brojNepogodenihSlova;
+        }
+    }
+
+    if (brojNepogodenihSlova == brojNepogodenihSlovaNaPocetku) {
+        brojPromasaja++;
+    }
 }
 
 document.getElementById("id1")
     .addEventListener("keypress", function(event) {
 
-    if (event.keyCode == 13) {
+        if (event.keyCode == 13) {
 
-        document.getElementById("id2").click();
-        event.preventDefault();
-         document.getElementById('id1').value = "";
-        console.log("hello5");
-    }
-});
+            document.getElementById("id2").click();
+            event.preventDefault();
+            document.getElementById('id1').value = "";
+
+        }
+    });
 
 
 
-var film = "The Warriors";
+var film = "skafisnjafskaf";
 
 var poljeNepogodenih = [];
 
 var brojNepogodenihSlova = film.length;
+
 
 for (var i = 0; i < film.length; i++) {
     poljeNepogodenih[i] = "_";
